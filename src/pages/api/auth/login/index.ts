9e4ef45659fb2baf397584549a2e75c09349162a
Jonathan <joohny_2014@hotmail.com>
2023-05-03 23:19:51 -0300

    feat: Login Function

---
+++ src/pages/api/auth/login/index.ts
@@ -0,0 +1,22 @@
+import { User } from "@/@types/User";
+import Handler from "@/back/handler";
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+class Login extends Handler {
+  async post(req: NextApiRequest, res: NextApiResponse) {
+    const prisma = new PrismaClient();
+    const { email, password }: User = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ error: "Email e Senha são necessários" });
+    } else {
+      try {
+        const user = await prisma.user.findFirst({ where: { email } });
+        return res.status(200).json({ user });
+      } catch (err) {
+        return res.status(400).json({ message: err });
+      }
+    }
+  }
+}
+export default new Login().handler;
