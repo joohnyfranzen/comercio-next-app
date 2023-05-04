18e36f2602536d09edaac8d296c323e34a618b57
Jonathan <joohny_2014@hotmail.com>
2023-05-04 13:43:46 -0300

    feat: ChakraUI added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import theme from "@/theme/chakra";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps: { pageProps } }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
---
+++ src/theme/chakra.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const config = {
+  enableColorModeControl: false,
+};
+const theme = extendTheme({
+  config,
+});
+
+export default theme;
