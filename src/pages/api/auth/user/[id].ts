1298c0c2c9fc9128f4272de6fd2dc9137c44b506
Jonathan <joohny_2014@hotmail.com>
2023-05-03 23:35:35 -0300

    feat: User get, update and delete functions

---
+++ src/pages/api/auth/user/[id].ts
@@ -0,0 +1,46 @@
+import Handler from "@/back/handler";
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+class User extends Handler {
+  async put(req: NextApiRequest, res: NextApiResponse) {
+    const prisma = new PrismaClient();
+    const updateBody = req.body;
+    const id = req.query.id;
+
+    try {
+      const user = await prisma.user.update({
+        where: { id: Number(id) },
+        data: updateBody,
+      });
+      return res.status(200).json({ user });
+    } catch (err) {
+      return res.status(400).json({ message: err });
+    }
+  }
+  async get(req: NextApiRequest, res: NextApiResponse) {
+    const prisma = new PrismaClient();
+    const id = req.query.id;
+    try {
+      const user = await prisma.user.findFirst({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json({ user });
+    } catch (err) {
+      return res.status(400).json({ message: err });
+    }
+  }
+  async delete(req: NextApiRequest, res: NextApiResponse) {
+    const prisma = new PrismaClient();
+    const id = req.query.id;
+    try {
+      const user = await prisma.user.delete({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json({ user });
+    } catch (err) {
+      return res.status(400).json({ message: err });
+    }
+  }
+}
+export default new User().handler;
