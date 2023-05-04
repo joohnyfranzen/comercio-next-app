71974393e579787ba339fbbeba4a2c928e11d253
Jonathan <joohny_2014@hotmail.com>
2023-05-03 23:31:42 -0300

    feat: Product delete function

---
+++ src/pages/api/product/[id].ts
@@ -0,0 +1,43 @@
+import Handler from "@/back/handler";
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+class Product extends Handler {
+  async put(req: NextApiRequest, res: NextApiResponse) {
+    const prisma = new PrismaClient();
+    const updateBody = req.body;
+    try {
+      const product = await prisma.product.create({
+        data: updateBody,
+      });
+      return res.status(200).json({ product });
+    } catch (err) {
+      return res.status(400).json({ message: err });
+    }
+  }
+  async get(req: NextApiRequest, res: NextApiResponse) {
+    const prisma = new PrismaClient();
+    const id = req.query.id;
+    try {
+      const product = await prisma.product.findFirst({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json({ product });
+    } catch (err) {
+      return res.status(400).json({ message: err });
+    }
+  }
+  async delete(req: NextApiRequest, res: NextApiResponse) {
+    const prisma = new PrismaClient();
+    const id = req.query.id;
+    try {
+      const product = await prisma.product.delete({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json({ product });
+    } catch (err) {
+      return res.status(400).json({ message: err });
+    }
+  }
+}
+export default new Product().handler;
