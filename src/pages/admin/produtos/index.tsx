e40243c852a2e3354decc293f40fdda154ae869e
Jonathan <joohny_2014@hotmail.com>
2023-05-05 20:31:13 -0300

    feat: Produtos Page

---
+++ src/pages/admin/produtos/index.tsx
@@ -0,0 +1,10 @@
+import Products from "@/components/AdminComponents/Products";
+import React from "react";
+
+export default function Produtos() {
+  return (
+    <div>
+      <Products />
+    </div>
+  );
+}
