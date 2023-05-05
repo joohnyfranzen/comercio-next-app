3132a76a2c569b92e58e03ae168d447e33d919c6
Jonathan <joohny_2014@hotmail.com>
2023-05-05 20:31:04 -0300

    feat: NovoUsuário Page

---
+++ src/pages/admin/novousuario/index.tsx
@@ -0,0 +1,10 @@
+import NewUser from "@/components/AdminComponents/NewUser";
+import React from "react";
+
+export default function NovoUsuário() {
+  return (
+    <div>
+      <NewUser />
+    </div>
+  );
+}
