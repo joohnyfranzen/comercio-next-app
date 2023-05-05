0ce01ed9189be9cbc6a33c2d0b206b0195e0dac7
Jonathan <joohny_2014@hotmail.com>
2023-05-05 20:30:49 -0300

    feat: NovaVenda Page

---
+++ src/pages/admin/novavenda/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NewSell from "@/components/AdminComponents/NewSell";
+
+export default function NovaVenda() {
+  return (
+    <div>
+      <NewSell />
+    </div>
+  );
+}
