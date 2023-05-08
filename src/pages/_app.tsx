c2ac4fd54c5b2dcf730aa0b825d7988ae133982f
Jonathan <joohny_2014@hotmail.com>
2023-05-08 20:00:21 -0300

    feat: Tailwind imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import theme from "@/theme/chakra";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps: { pageProps } }: AppProps) {
   return (
