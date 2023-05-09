c74ca7b4a74bbcdd61ad60dc808096a603a3b9f2
Jonathan <joohny_2014@hotmail.com>
2023-05-09 09:23:35 -0300

    fix: removind try catch for debbugging,  dont ask

--- src/pages/api/inventory/index.ts
+++ src/pages/api/inventory/index.ts
@@ -6,17 +6,14 @@ class Inventory extends Handler {
   async post(req: NextApiRequest, res: NextApiResponse) {
     const { productId, stock } = req.body;
     const prisma = new PrismaClient();
-    try {
-      const inventory = await prisma.inventory.create({
-        data: {
-          product: { connect: { id: productId } },
-          stock: stock,
-        },
-      });
-      return res.status(200).json({ inventory });
-    } catch (err) {
-      return res.status(400).json({ message: err });
-    }
+
+    const inventory = await prisma.inventory.create({
+      data: {
+        productId: String(productId),
+        stock: Number(stock),
+      },
+    });
+    return res.status(200).json({ inventory });
   }
 }
 export default new Inventory().handler;
