79c40abbea14b1c208f6ea191239896f3606048b
Jonathan <joohny_2014@hotmail.com>
2023-05-16 14:22:09 -0300

    feat: Reworking Sells

--- src/pages/admin/vendas/index.tsx
+++ src/pages/admin/vendas/index.tsx
@@ -1,7 +1,10 @@
 import { User } from "@/@types/User";
 import DeleteOrder from "@/components/AdminComponents/DelOrder";
+import EndOrder from "@/components/AdminComponents/EndOrder";
 import Layout from "@/components/AdminComponents/Layout";
+import { Button, Td } from "@chakra-ui/react";
 import axios from "axios";
+import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
@@ -16,30 +19,87 @@ export default function Vendas() {
 
   return (
     <Layout>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      Vendas
-      {data?.map((vendasPorUsuário) => {
-        let total = 0;
-        return (
-          <>
-            <h2>Compra de: {vendasPorUsuário.name}</h2>
-            {vendasPorUsuário.userProducts?.map((product) => {
-              total += product.product.price;
+      <h1 className="text-center font-bold text-2xl mb-5">Vendas</h1>
+      <Button colorScheme="facebook">
+        <Link className="flex m-5" href="/admin/novavenda">
+          Nova Venda
+        </Link>
+      </Button>
+      <div className="text-center justify-center flex flex-wrap">
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th className="px-5 py-3 border">Usuário</th>
+              <th className="px-5 py-3 border">Compra</th>
+              <th className="px-5 py-3 border">Total</th>
+              <th className="px-5 py-3 border">Detalhes</th>
+              <th className="px-5 py-3 border">Finalizar</th>
+              <th className="px-5 py-3 border">Deletar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map((vendasPorUsuário) => {
+              let total = 0;
               return (
-                <>
-                  <h2>Produto: {product.product.name}</h2>
-                  <h2>Valor: R${product.product.price},00</h2>
-                  <DeleteOrder id={product.id} />
-                </>
+                <tr key={vendasPorUsuário.id}>
+                  <td>{vendasPorUsuário.name}</td>
+                  <td className="px-5 py-3 border">
+                    {vendasPorUsuário.userProducts?.map((product) => {
+                      return (
+                        <>
+                          {product.product.name}
+                          <br />
+                        </>
+                      );
+                    })}
+                  </td>
+                  <td className="px-5 py-3 border">
+                    {vendasPorUsuário.userProducts?.map((product) => {
+                      total += product.product.price;
+                      return <></>;
+                    })}
+                    {total}
+                  </td>
+
+                  <td className="px-5 py-3 border">
+                    <Link href={`/admin/venda/${vendasPorUsuário.id}`}>
+                      <Button variant="solid" color="black" bgColor="green.200">
+                        Imprimir
+                      </Button>
+                    </Link>
+                  </td>
+
+                  <td className="px-5 py-3 border">
+                    {vendasPorUsuário.userProducts?.map((product) => {
+                      return (
+                        <>
+                          <EndOrder id={product.id} />
+                        </>
+                      );
+                    })}
+                  </td>
+                  <td className="px-5 py-3 border">
+                    {vendasPorUsuário.userProducts?.map((product) => {
+                      return (
+                        <>
+                          <EndOrder id={product.id} />
+                          <DeleteOrder id={product.id} />
+                        </>
+                      );
+                    })}
+                  </td>
+                </tr>
               );
             })}
-            <h2>Total: R${total},00</h2>
-            <Link href={`/admin/venda/${vendasPorUsuário.id}`}>
-              Ver Detalhes
-            </Link>
-          </>
-        );
-      })}
+          </tbody>
+        </table>
+      </div>
+
+      <Button colorScheme="facebook">
+        <Link className="flex m-5" href="/admin/novavenda">
+          Nova Venda{" "}
+        </Link>
+      </Button>
     </Layout>
   );
 }
