04fc99df78b3743fc556c01e8d211fe2cc707940
Jonathan <joohny_2014@hotmail.com>
2023-05-16 14:20:15 -0300

    feat: styiling Inputs

--- src/components/AdminComponents/NewUser/index.tsx
+++ src/components/AdminComponents/NewUser/index.tsx
@@ -37,67 +37,83 @@ export default function NewUser() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <Input
-          type="text"
-          name="name"
-          placeholder="Nome"
-          value={formData.name}
-          onChange={handleInputChange}
-        />
-        <Input
-          type="text"
-          name="email"
-          placeholder="E-mail"
-          value={formData.email}
-          onChange={handleInputChange}
-        />
-        <Input
-          type="text"
-          name="password"
-          placeholder="Senha"
-          value={formData.password}
-          onChange={handleInputChange}
-        />
-        <Input
-          type="text"
-          name="phoneNumber"
-          placeholder="Numero de telefone"
-          value={formData.phoneNumber}
-          onChange={handleInputChange}
-        />
-        <Input
-          type="text"
-          name="street"
-          placeholder="Endereço"
-          value={formData.address.street}
-          onChange={(event) =>
-            setFormData((prevFormData) => ({
-              ...prevFormData,
-              address: {
-                ...prevFormData.address,
-                street: event.target.value,
-              },
-            }))
-          }
-        />
-        <Input
-          type="text"
-          name="city"
-          defaultValue="Jaragua do Sul"
-          value={formData.address.city}
-          onChange={(event) =>
-            setFormData((prevFormData) => ({
-              ...prevFormData,
-              address: {
-                ...prevFormData.address,
-                city: event.target.value,
-              },
-            }))
-          }
-        />
-        <Button type="submit">Criar Usuário</Button>
+    <div className="flex justify-center my-5">
+      <form className="w-96 center" onSubmit={handleSubmit}>
+        <h1 className="text-2xl text-center">Adicionar Novo Usuário</h1>
+        <div>
+          <Input
+            className=" w-full p-2  mt-3 border-2 border-gray-200"
+            type="text"
+            name="name"
+            placeholder="Nome"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          <Input
+            className=" w-full p-2  mt-3 border-2 border-gray-200"
+            type="text"
+            name="email"
+            placeholder="E-mail / CPF"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+          <Input
+            className=" w-full p-2  mt-3 border-2 border-gray-200"
+            type="text"
+            name="password"
+            placeholder="Senha"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+          <Input
+            className=" w-full p-2  mt-3 border-2 border-gray-200"
+            type="text"
+            name="phoneNumber"
+            placeholder="Numero de telefone"
+            value={formData.phoneNumber}
+            onChange={handleInputChange}
+          />
+          <Input
+            className=" w-full p-2  mt-3 border-2 border-gray-200"
+            type="text"
+            name="street"
+            placeholder="Endereço"
+            value={formData.address.street}
+            onChange={(event) =>
+              setFormData((prevFormData) => ({
+                ...prevFormData,
+                address: {
+                  ...prevFormData.address,
+                  street: event.target.value,
+                },
+              }))
+            }
+          />
+          <Input
+            className=" w-full p-2  mt-3 border-2 border-gray-200"
+            type="text"
+            name="city"
+            defaultValue="Jaragua do Sul"
+            value={formData.address.city}
+            onChange={(event) =>
+              setFormData((prevFormData) => ({
+                ...prevFormData,
+                address: {
+                  ...prevFormData.address,
+                  city: event.target.value,
+                },
+              }))
+            }
+          />
+        </div>
+
+        <Button
+          colorScheme="facebook"
+          className=" w-full p-2  mt-3 border-2 border-gray-200"
+          type="submit"
+        >
+          Criar Usuário
+        </Button>
       </form>
     </div>
   );
