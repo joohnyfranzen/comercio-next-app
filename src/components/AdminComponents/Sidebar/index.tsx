4fa5671ac88b9b29d39005ba92b31ca7f18f2a1f
Jonathan <joohny_2014@hotmail.com>
2023-06-01 11:19:01 -0300

    fix: dashboard link

--- src/components/AdminComponents/Sidebar/index.tsx
+++ src/components/AdminComponents/Sidebar/index.tsx
@@ -17,7 +17,7 @@ export default function Sidebar() {
           </li>
           <li>
             <Link
-              href="/admin"
+              href="/admin/dashboard"
               className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
             >
               <span className="inline-flex justify-center items-center ml-4">
