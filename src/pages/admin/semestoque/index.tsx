5ca25c9b4d8fb319c317d69156641d1cea803c09
Jonathan <joohny_2014@hotmail.com>
2023-06-04 14:23:37 -0300

    feat: NoStockProducts

---
+++ src/pages/admin/semestoque/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/AdminComponents/Layout";
+import NoStockProducts from "@/components/AdminComponents/noStockProducts";
+import React from "react";
+
+export default function SemEstoque() {
+  return (
+    <Layout>
+      <NoStockProducts />
+    </Layout>
+  );
+}
