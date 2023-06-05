e5afda7b159884a39db32dbb772334959cf20767
Jonathan <joohny_2014@hotmail.com>
2023-06-05 19:46:27 -0300

    feat: Home Product Filters

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "http://moveis-eliane.com/api",
 });
 
 export default api;
